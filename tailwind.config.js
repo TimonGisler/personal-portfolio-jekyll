dce1e011dc4563ad680008d916fbffbd21ac40ff
Your Name <timongisler@icloud.com>
2022-07-17 19:08:53 +0200

    I think i managed to bring tailwind to work

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './index.html',
+    './_includes/**/*.{html,js}',
+    './_layout/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
